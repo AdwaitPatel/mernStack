84e9f34b24b17603f81e241c4f24a0ea029799c6
Adwait Patel <145041038+AdwaitPatel@users.noreply.github.com>
2026-02-06 15:17:14 +0530

    day14 primitive/non-prim import/export

--- NODEJS/day14/file.js
+++ NODEJS/day14/file.js
@@ -10,16 +10,21 @@
 // console.log("🚀 ~ multiplication : ", functions["multi"](3, 5))
 
 
-// using object destructuring to get the functions
-const { add, multi } = require("./utils")
-// const { add, multi } = functions;
-console.log("🚀 ~ add : ", add(3, 1))
-console.log("🚀 ~ multiplication : ", multi(3, 5))
+// using "Object Destructuring" to get the functions
+// const { add, multi } = require("./utils")
+// // const { add, multi } = functions;
+// console.log("🚀 ~ add : ", add(3, 1))
+// console.log("🚀 ~ multiplication : ", multi(3, 5))
 
 
+// ==== require("./utils") => here the priority is [.js > json > folder]
+// As after utils no extension is mentioned it can be anything(.js, .json, dir)
 
+// =======================================================
 
-
+let val = require("./counter")
+val.push(5);
+console.log(val)
 
 
 
